e217861bfc574ab4772a4d25b2763575048d2701
Kelsey Gilmore-Innis <kelseyq@gmail.com>
2014-08-05 09:21:55 -0700

    add mentions and number replies

--- src/clojure_twitter_bot/core.clj
+++ src/clojure_twitter_bot/core.clj
@@ -66,13 +66,14 @@
 (defn extract-tweet-info
   [tweetMap]
     {:tweet (:text tweetMap),
-     :tweet_link (str "http://twitter.com/" (get-in tweetMap [:user :screen_name]) "/status/" (:id_str tweetMap)),
+     :tweet_link (str "https://twitter.com/" (get-in tweetMap [:user :screen_name]) "/status/" (:id_str tweetMap)),
      :screen-name (get-in tweetMap [:user :screen_name])
      :id_str (:id_str tweetMap)})
 
 (defn now [] (java.util.Date.))
 
 (def whos ["who" "whoo" "whooo" "whoooo" "whooooo" "whooooo" "whoooooo" "whooooooo" "whoooooooo" "whooooooooo?" "whoooooooooo"])
+(def numbers ["281 330 8004" "281-330-8004" "(281)330-8004" "(281) 330-8004"])
 
 (comment (re-find #"(?is)^who+\?+[^a-zA-Z0-9\s]*" (clojure.string/trim (:tweet tweetMap))))
 
@@ -88,6 +89,12 @@
 (def who-stream (client/create-twitter-stream twitter.api.streaming/statuses-filter
                                           :oauth-creds my-creds :params {:track (clojure.string/join "," whos)}))
 
+(def mentions-stream (client/create-twitter-stream twitter.api.streaming/user-stream
+                                              :oauth-creds my-creds :params {:with "user"}))
+
+(def number-stream (client/create-twitter-stream twitter.api.streaming/statuses-filter
+                                              :oauth-creds my-creds :params {:track (clojure.string/join "," numbers)}))
+
 (defn do-every
   [ms callback]
   (loop []
@@ -97,7 +104,7 @@
            (catch Exception e (log/error e (str "caught exception: " (.getMessage e))))))
     (recur)))
 
-(def interval 30)
+(def mike-jones-interval 30)
 (def empty-state {:candidates [] :minutes-since-update 0})
 
 (defn mike-jones
@@ -106,20 +113,23 @@
   (defn reset-state
     [who response]
     (if (== (quot (:code (ac/status response)) 100) 2)
-      (do (reset! state empty-state)
-          (swap! previous conj who))
+      (do
+          (reset! state empty-state)
+          (swap! previous conj (:who who)))
       (log/error "error posting tweet, response code " (:code (ac/status response)) ": " (ac/string response))))
 
   (if-let [candidates (seq (:candidates @state))]
     (let [who (rand-nth candidates)
-          tweet (str (:who who) "\nMIKE JONES\n\n" (:url who))]
+          tweet (str (:who who) "\nMIKE JONES\n" (:url who))]
       (log/debug (str "MIKE JONES-ING " who))
       (statuses-update :oauth-creds my-creds
-                       :params {:status tweet},
+                       :params {:status tweet
+                                :lat 29.804295
+                                :long -95.388141},
                        :callbacks (SyncSingleCallback. (partial reset-state who)
                                                        response-throw-error
                                                        exception-rethrow)))
-    (log/debug (str "no candidates found after " (:minutes-since-update @state) " minutes"))))
+    (log/debug (str "no mike jones candidates found after " (:minutes-since-update @state) " minutes"))))
 
 (defn log-size
   [coll]
@@ -139,28 +149,136 @@
                                         (filter #(not (.contains (:who %) "http")))
                                         (filter #(< (count (:who %)) 104))))]
             (do
-              (log/debug (str "adding " (count candidates) " candidates to list:\n" (clojure.string/join "\n" (map :who candidates))))
+              (log/debug (str "adding " (count candidates) " candidates to mike jones list:\n" (clojure.string/join "\n" (map :who candidates))))
               (swap! state update-in [:candidates] concat candidates))
-            (log/debug "no candidates found on regular interval check"))
-          (if (>= (:minutes-since-update @state) interval)
+            (log/debug "no candidates found on regular mike jones interval check"))
+          (if (>= (:minutes-since-update @state) mike-jones-interval)
             (mike-jones state previous)))
+
+(comment            (= (:in_reply_to_user_id_str tweetMap) "2678831582"))
+
+(defn is-legit-mention?
+  [tweetMap]
+       (not (or (empty? tweetMap)
+                (= (get-in tweetMap [:user :screen_name]) "NowImBot")
+                (and (= (get-in tweetMap [:retweeted_status :user :screen_name]) "NowImBot")
+                     (.startsWith (get-in tweetMap [:retweeted_status :text]) "@")))))
+
+(def replies
+  ["back then % didn't want me, now I'm hot % all on me"
+   "281 330 8004 hit Mike Jones up on the low cause Mike Jones about to blow"
+   "still tweetin on fo' fo's, wrapped in fo' vogues"])
+
+(defn get-mention-reply
+  [tweetMap]
+  (if (= (get-in tweetMap [:retweeted_status :user :screen_name]) "NowImBot")
+    (clojure.string/replace (rand-nth replies) #"%" name)
+    (let [text (clojure.string/lower-case (clojure.string/replace (:text tweetMap) #"([\"|“].*[\"|”])" ""))]
+       (cond (.contains text "mike") "WHO?"
+             (.contains text "who") "MIKE JONES"
+             :else (clojure.string/replace (rand-nth replies) #"%" name)))))
+
+(defn reply-to-mention
+  [mention]
+  (let [name (str "@" (get-in mention [:user :screen_name]))
+        tweet-text (get-mention-reply mention)
+        tweet (str name " " tweet-text) ]
+    (log/debug (str "replying to mention from " name))
+    (try
+      (statuses-update :oauth-creds my-creds
+                       :params {:status tweet
+                                :in_reply_to_status_id (:id_str mention)
+                                :lat 29.804295
+                                :long -95.388141},
+                       :callbacks (SyncSingleCallback. response-return-body
+                                                       response-throw-error
+                                                       exception-rethrow))
+      (catch Exception e (log/error e (str "error replying to mention from " (get-in mention [:user :screen_name])))))))
+
+(defn reply-to-mentions
+  []
+  (when-let [mentions (seq (->> (:tweet (client/retrieve-queues mentions-stream))
+                                 (filter is-legit-mention?)))]
+    (doseq [m mentions] (reply-to-mention m))))
+
+(defn reply-to-number
+  [number-tweet]
+  (log/debug (str "replying to number from " (:screen-name number-tweet)))
+  (try
+      (statuses-update :oauth-creds my-creds
+                       :params {:status (str "@" (:screen-name number-tweet) " hit Mike Jones up on the low cause Mike Jones about to blow")
+                                :in_reply_to_status_id (:id_str number-tweet)
+                                :lat 29.804295
+                                :long -95.388141},
+                       :callbacks (SyncSingleCallback. response-return-body
+                                                       response-throw-error
+                                                       exception-rethrow))
+      (catch Exception e (log/error e (str "error replying to number from " (:screen-name number-tweet))))))
+
+
+(defn reply-to-numbers
+  []
+  (when-let [number (seq (->> (:tweet (client/retrieve-queues number-stream))
+                                (filter candidate?)
+                                (map extract-tweet-info)
+                                (filter #(not (.contains (clojure.string/lower-case (:tweet %)) "mike")))
+                                (filter #(not (.contains (clojure.string/lower-case (:tweet %)) "jones")))
+                                ))]
+    (doseq [n number] (reply-to-number n))))
+
+(defn reply-to-follow
+  [follow]
+  (let [name (str "@" (get-in follow [:source :screen_name]))
+        tweet-text (clojure.string/replace (rand-nth replies) #"%" name)
+        tweet (str name " " tweet-text) ]
+    (log/debug (str "replying to follow from " name))
+    (try
+      (statuses-update :oauth-creds my-creds
+                       :params {:status tweet
+                                :lat 29.804295
+                                :long -95.388141},
+                       :callbacks (SyncSingleCallback. response-return-body
+                                                       response-throw-error
+                                                       exception-rethrow))
+      (catch Exception e (log/error e (str "error replying to follow from " (get-in follow [:source :screen_name])))))))
+
+
+(defn reply-to-follows
+  []
+  (when-let [follows (seq (->> (:unknown (client/retrieve-queues mentions-stream))
+                               (filter #(= (:event %) "follow"))))]
+    (doseq [f follows] (reply-to-follow f))))
+
 
 (defn -main
   []
-  (let [state (atom (assoc empty-state :minutes-since-update interval))
+  (let [mike-jones-state (atom (assoc empty-state :minutes-since-update mike-jones-interval))
         previous (atom #{})]
 
-    (defn run-bot
+    (defn respond-who
       []
       (do
-        (swap! state #(update-in % [:minutes-since-update] inc))
-        (check-for-who state previous)
+        (swap! mike-jones-state #(update-in % [:minutes-since-update] inc))
+        (check-for-who mike-jones-state previous)
         ))
 
-    (do
       (log/debug "STARTING MIKE JONES BOT")
+
+      (client/start-twitter-stream mentions-stream)
+      (client/start-twitter-stream number-stream)
       (client/start-twitter-stream who-stream)
-      (doto
-          (Thread. (do-every 60000 run-bot))
-        (.setDaemon true)
-        (.start)))))
+
+        (future (log/debug "STARTING MENTIONS STREAM")
+                       (do-every 60500 reply-to-mentions))
+
+        (future (log/debug "STARTING NUMBERS STREAM")
+                       (do-every 60250 reply-to-numbers))
+
+        (future (log/debug "STARTING WHO STREAM")
+                       (do-every 60000 respond-who))
+
+        (future (log/debug "STARTING FOLLOW STREAM")
+              (do-every 60750 reply-to-follows))
+
+
+      ))
